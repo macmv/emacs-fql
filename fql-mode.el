c7c35c96cd3de51ecff5e458cf933eba0590c401
Neil Macneale V <neil.macneale.v@gmail.com>
2023-11-12 16:21:03 -0800

    Add in field names and comment out some unused things

--- fql-mode.el
+++ fql-mode.el
@@ -37,6 +37,12 @@
     "Number" "Int" "Long" "Double" "Float" "Object" "Set"
     "SetCursor" "Struct" "Time" "TransactionTime" "Query"))
 
+(defun fql-re-word (inner) (concat "\\<" inner "\\>"))
+(defun fql-re-grab (inner) (concat "\\(" inner "\\)"))
+(defun fql-re-shy (inner) (concat "\\(?:" inner "\\)"))
+
+(defconst fql-re-ident "[[:word:][:multibyte:]_][[:word:][:multibyte:]_[:digit:]]*")
+
 (defvar fql-font-lock-keywords
   `(
     ; Keywords proper
@@ -45,50 +51,14 @@
     ; Modules
     (,(regexp-opt fql-modules 'symbols) . font-lock-constant-face)
 
-    ;;; Contextual keywords
-    ;("\\_<\\(default\\)[[:space:]]+fn\\_>" 1 font-lock-keyword-face)
-    ;(,rust-re-union 1 font-lock-keyword-face)
-
-    ;;; Special types
-    ;(,(regexp-opt rust-special-types 'symbols) . font-lock-type-face)
-
-    ;;; The unsafe keyword
-    ;("\\_<unsafe\\_>" . 'rust-unsafe)
-
+    ; TODO: Need this for @role in FSL
     ;;; Attributes like `#[bar(baz)]` or `#![bar(baz)]` or `#[bar = "baz"]`
     ;(,(rust-re-grab (concat "#\\!?\\[" rust-re-ident "[^]]*\\]"))
     ; 1 font-lock-preprocessor-face keep)
 
-    ;;; Builtin formatting macros
-    ;(,(concat (rust-re-grab
-    ;           (concat (rust-re-word (regexp-opt rust-builtin-formatting-macros))
-    ;                   "!"))
-    ;          rust-formatting-macro-opening-re
-    ;          "\\(?:" rust-start-of-string-re "\\)?")
-    ; (1 'rust-builtin-formatting-macro)
-    ; (rust-string-interpolation-matcher
-    ;  (rust-end-of-string)
-    ;  nil
-    ;  (0 'rust-string-interpolation t nil)))
-
-    ;;; write! macro
-    ;(,(concat (rust-re-grab (concat (rust-re-word "write\\(ln\\)?") "!"))
-    ;          rust-formatting-macro-opening-re
-    ;          "[[:space:]]*[^\"]+,[[:space:]]*"
-    ;          rust-start-of-string-re)
-    ; (1 'rust-builtin-formatting-macro)
-    ; (rust-string-interpolation-matcher
-    ;  (rust-end-of-string)
-    ;  nil
-    ;  (0 'rust-string-interpolation t nil)))
-
-    ;;; Syntax extension invocations like `foo!`, highlight including the !
-    ;(,(concat (rust-re-grab (concat rust-re-ident "!")) "[({[:space:][]")
-    ; 1 font-lock-preprocessor-face)
-
-    ;;; Field names like `foo:`, highlight excluding the :
-    ;(,(concat (rust-re-grab rust-re-ident) "[[:space:]]*:[^:]")
-    ; 1 font-lock-variable-name-face)
+    ;; Field names like `foo:`, highlight excluding the :
+    (,(concat (fql-re-grab fql-re-ident) "[[:space:]]*:[^:]")
+     1 font-lock-variable-name-face)
 
     ;;; CamelCase Means Type Or Constructor
     ;(,rust-re-type-or-constructor 1 font-lock-type-face)
@@ -99,23 +69,7 @@
     ;          "\\_>")
     ; 1 font-lock-variable-name-face)
 
-    ;;; Type names like `Foo::`, highlight excluding the ::
-    ;(,(rust-path-font-lock-matcher rust-re-uc-ident) 1 font-lock-type-face)
-
-    ;;; Module names like `foo::`, highlight excluding the ::
-    ;(,(rust-path-font-lock-matcher rust-re-lc-ident) 1 font-lock-constant-face)
-
-    ;;; Lifetimes like `'foo`
-    ;(,(concat "'" (rust-re-grab rust-re-ident) "[^']") 1 font-lock-variable-name-face)
-
-    ;;; Question mark operator
-    ;("\\?" . 'rust-question-mark)
-    ;("\\(&+\\)\\(?:'\\(?:\\<\\|_\\)\\|\\<\\|[[({:*_|]\\)"
-    ; 1 'rust-ampersand-face)
-    ;;; Numbers with type suffix
-    ;(,rust-number-with-type 1 font-lock-type-face)
-    ;)
-
+   ; TODO: Need this for FSL
    ;; Ensure we highlight `Foo` in `struct Foo` as a type.
    ;(mapcar #'(lambda (x)
    ;            (list (rust-re-item-def (car x))
